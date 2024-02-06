2ca129c64c78f350f9392eefbf74f35bde66594e
Janson <jansonleong1221@gmail.com>
2024-02-07 07:53:55 +0800

    consoloe

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express();
 
 // Define a route for the root path
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+    try {
+      // Your route logic here
+      res.send('Hello, world!');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
 // Other routes and server setup...
 
