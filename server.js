b67b0f14f2f379c8d7a9630288489b4b9fda12fa
Janson <jansonleong1221@gmail.com>
2024-02-07 04:24:16 +0800

    test

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 
-// Define a route for the root path
 app.get('/', (req, res) => {
     res.send('Hello, this is your root route!');
 });
 
 // Other routes can be defined here...
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
