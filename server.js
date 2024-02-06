74f7009d6fa8cded3544c82d58b9c9cf9301307e
Janson <jansonleong1221@gmail.com>
2024-02-07 04:37:23 +0800

    test

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello, this is your root route!');
-});
-
-// Other routes can be defined here...
+const app = require("express")();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+app.get("", (req, res) => {
+  res.send("Hello world");
 });
+
+app.listen(PORT, () => {
+    console.log(`App up at port ${PORT}`);
+  });
